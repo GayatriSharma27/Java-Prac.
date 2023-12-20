e57cbc800482dd4f171c9c0dc6d01ec46dc50be1
Gayatri Sharma <gsharma100@myseneca.ca>
2023-12-19 21:14:46 -0500

    ++, -- ops

---
+++ src/Encrypt.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Encrypt {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your grades, I encrypt and decrypt it :=>");
+        int grade = sc.nextInt();
+
+        System.out.println("Encrypted grade: ");
+
+    }
+
+}
